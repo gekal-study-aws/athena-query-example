7f0989f1dcfff8c477c7db640089562994360d5c
gekal <liu.hongying@hotmail.com>
2026-01-02 21:12:19 +0900

    init commit

--- infra-sdk/lib/infra-sdk-stack.ts
+++ infra-sdk/lib/infra-sdk-stack.ts
@@ -1,16 +1,88 @@
-import * as cdk from 'aws-cdk-lib/core';
+import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as glue from 'aws-cdk-lib/aws-glue';
+import * as athena from 'aws-cdk-lib/aws-athena';
 
 export class InfraSdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // 1. 監査ログ保存用のS3バケット
+    const auditLogBucket = new s3.Bucket(this, 'AuditLogBucket', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      enforceSSL: true,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfraSdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // 2. Athenaのクエリ結果保存用のS3バケット
+    const queryResultBucket = new s3.Bucket(this, 'QueryResultBucket', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      enforceSSL: true,
+    });
+
+    // 3. Glue Database
+    const databaseName = 'audit_log_db';
+    new glue.CfnDatabase(this, 'AuditLogDatabase', {
+      catalogId: cdk.Stack.of(this).account,
+      databaseInput: {
+        name: databaseName,
+        description: 'Database for audit logs',
+      },
+    });
+
+    // 4. Glue Table (監査ログのスキーマ定義)
+    const tableName = 'audit_logs';
+    new glue.CfnTable(this, 'AuditLogTable', {
+      catalogId: cdk.Stack.of(this).account,
+      databaseName: databaseName,
+      tableInput: {
+        name: tableName,
+        description: 'Table for audit logs',
+        tableType: 'EXTERNAL_TABLE',
+        parameters: {
+          'has_encrypted_data': 'false',
+          'classification': 'json',
+          'typeOfData': 'file',
+        },
+        storageDescriptor: {
+          columns: [
+            { name: 'timestamp', type: 'string' },
+            { name: 'user_id', type: 'string' },
+            { name: 'event_name', type: 'string' },
+            { name: 'resource_id', type: 'string' },
+            { name: 'status', type: 'string' },
+            { name: 'ip_address', type: 'string' },
+          ],
+          location: auditLogBucket.s3UrlForObject(),
+          inputFormat: 'org.apache.hadoop.mapred.TextInputFormat',
+          outputFormat: 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat',
+          serdeInfo: {
+            serializationLib: 'org.openx.data.jsonserde.JsonSerDe',
+          },
+        },
+      },
+    });
+
+    // 5. Athena Workgroup
+    new athena.CfnWorkGroup(this, 'AuditLogWorkGroup', {
+      name: 'AuditLogWorkGroup',
+      workGroupConfiguration: {
+        resultConfiguration: {
+          outputLocation: queryResultBucket.s3UrlForObject(),
+        },
+      },
+    });
+
+    // Outputs
+    new cdk.CfnOutput(this, 'AuditLogBucketName', {
+      value: auditLogBucket.bucketName,
+    });
+    new cdk.CfnOutput(this, 'QueryResultBucketName', {
+      value: queryResultBucket.bucketName,
+    });
   }
 }
