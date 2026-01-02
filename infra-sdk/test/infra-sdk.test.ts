eab76d8d1bbc63f042ec16b6a4dade36af18f65f
gekal <liu.hongying@hotmail.com>
2026-01-02 20:55:39 +0900

    init commit

---
+++ infra-sdk/test/infra-sdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as InfraSdk from '../lib/infra-sdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/infra-sdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new InfraSdk.InfraSdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
