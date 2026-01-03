06ffac37a68a2eeb0650da1f39f17a24ee810565
gekal <liu.hongying@hotmail.com>
2026-01-03 09:10:35 +0900

    init commit for app

---
+++ app/src/main/java/cn/gekal/sample/awsathenaapidemo/AwsAthenaApiDemoApplication.java
@@ -0,0 +1,13 @@
+package cn.gekal.sample.awsathenaapidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsAthenaApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsAthenaApiDemoApplication.class, args);
+	}
+
+}
