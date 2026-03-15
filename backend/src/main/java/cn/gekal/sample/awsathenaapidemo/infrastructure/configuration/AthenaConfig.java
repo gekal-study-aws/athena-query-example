867f498852f3b28b6364766b9d8a4ef606c6d720
gekal <liu.hongying@hotmail.com>
2026-03-15 13:18:06 +0900

    enable AWS profile-based credentials: update `compose.yaml`, Dockerfile, and Spring configuration to support profile-based authentication for Athena and S3 services

--- backend/src/main/java/cn/gekal/sample/awsathenaapidemo/infrastructure/configuration/AthenaConfig.java
+++ backend/src/main/java/cn/gekal/sample/awsathenaapidemo/infrastructure/configuration/AthenaConfig.java
@@ -3,6 +3,9 @@ package cn.gekal.sample.awsathenaapidemo.infrastructure.configuration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.athena.AthenaClient;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
@@ -13,13 +16,30 @@ public class AthenaConfig {
   @Value("${aws.athena.region:ap-northeast-1}")
   private String region;
 
+  @Value("${aws.profile:default}")
+  private String profile;
+
   @Bean
-  public AthenaClient athenaClient() {
-    return AthenaClient.builder().region(Region.of(region)).build();
+  public AwsCredentialsProvider awsCredentialsProvider() {
+    if (profile != null && !profile.isEmpty() && !"default".equals(profile)) {
+      return ProfileCredentialsProvider.create(profile);
+    }
+    return DefaultCredentialsProvider.create();
   }
 
   @Bean
-  public S3Presigner s3Presigner() {
-    return S3Presigner.builder().region(Region.of(region)).build();
+  public AthenaClient athenaClient(AwsCredentialsProvider credentialsProvider) {
+    return AthenaClient.builder()
+        .region(Region.of(region))
+        .credentialsProvider(credentialsProvider)
+        .build();
+  }
+
+  @Bean
+  public S3Presigner s3Presigner(AwsCredentialsProvider credentialsProvider) {
+    return S3Presigner.builder()
+        .region(Region.of(region))
+        .credentialsProvider(credentialsProvider)
+        .build();
   }
 }
